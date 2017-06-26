e8fc18eeb0833382092437f4c942241bbf3d0a36
acosorio <anderson.junqueira@gmail.com>
2017-06-26 06:22:16 -0300

    ajustes para o admin

--- src/main/java/br/com/ertic/util/infraestructure/service/RestFullService.java
+++ src/main/java/br/com/ertic/util/infraestructure/service/RestFullService.java
@@ -1,11 +1,17 @@
 package br.com.ertic.util.infraestructure.service;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ertic.util.infraestructure.exception.InternalException;
 
 public class RestFullService<E, PK extends Serializable> {
 
@@ -25,8 +31,57 @@ public class RestFullService<E, PK extends Serializable> {
         loadTypes();
     }
 
+    protected JpaRepository<E, PK> getRepository() {
+        return repository;
+    }
+
     public List<E> findAll() {
         return repository.findAll();
+    }
+
+    public List<E> findAll(Example<E> example) {
+        return repository.findAll(example);
+    }
+
+    public List<E> findAll(Map<String, String[]> params) {
+        try {
+            E obj = modelClass.newInstance();
+
+            for(String key : params.keySet()) {
+                if(params.get(key).length != 1) {
+                    continue;
+                }
+
+                Method m = getMethodFromProperty(key);
+                if(m != null) {
+                    if(m.getParameterTypes()[0].isInstance(Long.class)) {
+                        m.invoke(obj, Long.parseLong(params.get(key)[0]));
+
+                    } else if(m.getParameterTypes()[0].isInstance(Double.class)) {
+                        m.invoke(obj, Double.parseDouble(params.get(key)[0]));
+
+                    } else if(m.getParameterTypes()[0].isInstance(String.class)) {
+                        m.invoke(obj, params.get(key)[0] + "%");
+                    }
+                }
+
+            }
+
+            return repository.findAll(Example.of(obj));
+
+        } catch(IllegalAccessException | InstantiationException | InvocationTargetException ex) {
+            throw new InternalException("problemas-classe-findall", ex);
+        }
+    }
+
+    private Method getMethodFromProperty(String property) {
+        String mName = "set" + property.substring(0, 1).toUpperCase() + property.substring(1);
+        for(Method m : modelClass.getMethods()) {
+            if(m.getName().equals(mName) && m.getParameterTypes().length == 1) {
+                return m;
+            }
+        }
+        return null;
     }
 
     public E findOne(PK id) {
--- src/main/java/br/com/ertic/util/infraestructure/web/RestFullEndpoint.java
+++ src/main/java/br/com/ertic/util/infraestructure/web/RestFullEndpoint.java
@@ -3,6 +3,8 @@ package br.com.ertic.util.infraestructure.web;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +22,14 @@ public class RestFullEndpoint<E, PK extends Serializable> {
         this.service = service;
     }
 
+    protected RestFullService<E, PK> getService() {
+        return service;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<E>> getAll() {
-        return new ResponseEntity<>(service.findAll(),HttpStatus.OK);
+    public ResponseEntity<List<E>> getAll(HttpServletRequest request) {
+        return new ResponseEntity<>(
+            service.findAll(request.getParameterMap()),HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
